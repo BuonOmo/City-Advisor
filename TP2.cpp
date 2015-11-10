ad581b3a93b4d0b831ec7ecd4a889543464e8928
BuonOmo <buonomo.ulysse@gmail.com>
2015-11-10 01:17:38 +0100

    ajout de JAM_D7 et debug d’EXIT

--- TP2.cpp
+++ TP2.cpp
@@ -17,71 +17,71 @@ using namespace std;
 
 int main(void)
 {
-   int id, yyyy, mm, dd, h, m, d7;
-   char trafic;
-   string str;
+	int id, yyyy, mm, dd, h, m, d7;
+	char trafic;
+	string str, line;
 
-   City LA;
+	City LA;
 
-   /* placer ici la déclaration de votre liste ainsi que
-    * son initialisation */
-
-
-
-   getline(cin, str,' ');
-   while (str.compare("bye")!=0) {
-      if (str.compare("ADD")==0) {
-    	  getline(cin, str, ' ');
-    	  id = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-    	  yyyy = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-    	  mm = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-    	  dd = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-    	  h = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-    	  m = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str, ' ');
-
-    	  d7 = atoi(str.c_str());
-    	  cout << str << endl;
-
-    	  getline(cin, str);
-    	  trafic = str[0];
-    	  cout << str << endl;
-
-    	  LA.addEvent(id, yyyy, mm, dd, h, m, d7, trafic);
-
-      } else if (str.compare("JAM_DH")==0) {
-
-      } else if (str.compare("STATS_D7")==0)
-      {
-    	  cout << "statD7"<<endl;
-    	  getline(cin, str);
-    	  LA.statDay(atoi(str.c_str()));
-      } else if (str.compare("STATS_C")==0) {
-    	  getline(cin, str);
-    	  LA.statSensor(atoi(str.c_str()));
-      }
-
-      str.clear();
-      getline(cin,str,' ');
-   }
+	/* placer ici la déclaration de votre liste ainsi que
+	* son initialisation */
 
 
-   return 0;
+
+
+	while (cin.peek() != 'E') {
+		getline(cin, str,' ');
+		if (str.compare("ADD")==0) {
+			getline(cin, str, ' ');
+			id = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+			yyyy = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+			mm = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+			dd = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+			h = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+			m = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str, ' ');
+
+			d7 = atoi(str.c_str());
+			//cout << "#" << str << endl;
+
+			getline(cin, str);
+			trafic = str[0];
+			//cout << "#" << str << endl;
+
+			LA.addEvent(id, yyyy, mm, dd, h, m, d7, trafic);
+
+		} else if (str.compare("JAM_DH")==0) {
+			getline(cin, str);
+			LA.jamDay(atoi(str.c_str()));
+		} else if (str.compare("STATS_D7")==0)
+		{
+			getline(cin, str);
+			LA.statDay(atoi(str.c_str()));
+		} else if (str.compare("STATS_C")==0) {
+			getline(cin, str);
+			LA.statSensor(atoi(str.c_str()));
+		}
+
+		str.clear();
+	}
+
+
+	return 0;
 }
